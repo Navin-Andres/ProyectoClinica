cf2012fb761e136f8bd6ae5aca6aa0e3fa391e8e
Navin <navinbalmaceda83@gmail.com>
2026-08-14 09:44:56 -0500

    Configurar despliegue en Render

--- models/db.py
+++ models/db.py
@@ -2,14 +2,29 @@ import pymysql
 from pymysql.cursors import DictCursor
 import sys
 import os
+import ssl
 
 # Añadir directorio padre al path para importar settings
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from settings import MYSQL_HOST, MYSQL_PORT, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB
 
+
+def get_ssl_context():
+    """Crea una conexión TLS para proveedores administrados como Aiven."""
+    use_ssl = os.getenv("MYSQL_SSL", "false").lower() in {"1", "true", "yes"}
+    if not use_ssl:
+        return None
+
+    context = ssl.create_default_context()
+    ca_file = os.getenv("MYSQL_SSL_CA")
+    if ca_file:
+        context.load_verify_locations(cafile=ca_file)
+    return context
+
+
 def get_connection():
-    return pymysql.connect(
+    options = dict(
         host=MYSQL_HOST,
         port=MYSQL_PORT,
         user=MYSQL_USER,
@@ -18,3 +33,7 @@ def get_connection():
         connect_timeout=10,
         cursorclass=DictCursor
     )
+    ssl_context = get_ssl_context()
+    if ssl_context:
+        options["ssl"] = ssl_context
+    return pymysql.connect(**options)
